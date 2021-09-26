9c33f4e0b8f175164aee10e91e29341738b013af
l3onardodev.github.io <leonardo.contato03@gmail.com>
2021-09-25 22:53:23 -0300

    Project's section almost done

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 const presentationScreen = document.querySelector(".presentation-screen");
 const portfolioContainer = document.querySelector(".portfolio-container");
+const headerNavContainer = document.querySelector(".header-menu-container");
 
 setTimeout(function () {
     portfolioContainer.style.height = "100vh";
@@ -8,3 +9,9 @@ setTimeout(function () {
 setTimeout(function () {
     portfolioContainer.style.height = "auto";
 }, 400); //3800
+
+window.addEventListener("scroll", function () {
+    console.log(headerNavContainer.offsetHeight);
+    console.log(window.window.scrollY);
+    console.log(document.body.clientHeight);
+});
