566a9292989719368020f64e8279696309efca00
l3onardodev.github.io <leonardo.contato03@gmail.com>
2021-09-26 19:07:56 -0300

    Icon that takes to top of the page, added.

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,14 @@ const presentationScreen = document.querySelector(".presentation-screen");
 const portfolioContainer = document.querySelector(".portfolio-container");
 const headerNavContainer = document.querySelector(".header-menu-container");
 
+document.addEventListener("scroll", function () {
+    if (document.querySelector(".header-menu-container").getBoundingClientRect().y < -22) {
+        document.querySelector(".scroll-top-icon-container").style.display = "flex";
+    } else {
+        document.querySelector(".scroll-top-icon-container").style.display = "none";
+    }
+});
+
 setTimeout(function () {
     portfolioContainer.style.height = "100vh";
 }, 100); //2300
