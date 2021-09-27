d919862ef2d6b703ef661771f8733fe2bab65cfa
l3onardodev.github.io <leonardo.contato03@gmail.com>
2021-09-27 15:38:16 -0300

    Services section partially done

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,12 @@ const presentationScreen = document.querySelector(".presentation-screen");
 const portfolioContainer = document.querySelector(".portfolio-container");
 const headerNavContainer = document.querySelector(".header-menu-container");
 
+const serviceItems = document.querySelectorAll(".service-item-container");
+const serviceItemTitle = document.querySelectorAll(".service-item-title");
+const serviceItemTitle2 = document.querySelectorAll(".service-item-title-2");
+
+addListenersToServiceItems();
+
 document.addEventListener("scroll", function () {
     if (document.querySelector(".header-menu-container").getBoundingClientRect().y < -22) {
         document.querySelector(".scroll-top-icon-container").style.display = "flex";
@@ -23,3 +29,19 @@ window.addEventListener("scroll", function () {
     console.log(window.window.scrollY);
     console.log(document.body.clientHeight);
 });
+
+function addListenersToServiceItems() {
+    console.log(serviceItems);
+    serviceItems.forEach((element, index) => {
+        element.addEventListener("mouseover", function () {
+            // document.querySelector(".service-item-title").style.animation = "text-out .5s";
+            serviceItemTitle[index].style.display = "none";
+            serviceItemTitle2[index].style.display = "block";
+        });
+
+        element.addEventListener("mouseout", function () {
+            serviceItemTitle[index].style.display = "block";
+            serviceItemTitle2[index].style.display = "none";
+        });
+    });
+}
